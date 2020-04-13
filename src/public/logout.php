4abafb7abb76d43fee3bd4e3b4e7b6a935fd456b
Leonardo Scapinello <leonardo.scapinello@outlook.com>
2020-04-13 15:51:24 -0300

    chore: recovery password and logout

--- src/public/logout.php
+++ src/public/logout.php
@@ -1,11 +1,8 @@
 <?php
 require_once("../properties/index.php");
 if ($session->isLogged()) {
-
     $session->cleanSession();
-
-    header("location: " . SITE_URL);
-    die;
-
-
 }
+
+header("location: " . SITE_URL);
+die;
